4007ad9e6c3e80bee892bce01d4ee00bf36e18af
Sara Harvey <saraharveynyc@gmail.com>
2018-08-30 16:39:07 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,36 +1,14 @@
 class Person
  
-   def initialize(name)
+  def initialize(name)
     @name = name
   end
 
- 
   def name
     @name
   end
- 
-  def breed
-    @breed 
-  end
- 
-  def name=(new_name)
-    @name = new_name
-  end
- 
- def breed=(new_breed)
-   @breed = new_breed
-  end 
-
+  
 end
 
-fido = Dog.new
+Joe = Person.new("Joe")
 fido.name
-
-fido.name = "Fido"
-fido.name
-
-snoopy = Dog.new
-snoopy.breed 
-
-snoopy.breed = "Beagle"
-snoopy.breed
